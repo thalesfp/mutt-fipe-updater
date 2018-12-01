83fff0a8825689fa292b420d87ac4ca86d3b0c5f
Thales Pereira <thalesfp@gmail.com>
2018-12-01 14:50:43 -0200

    lint index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
-import 'reflect-metadata';
-import * as dotenv from 'dotenv';
-import { createConnection } from 'typeorm';
-import * as Sentry from '@sentry/node';
+import * as Sentry from "@sentry/node";
+import * as dotenv from "dotenv";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 
-import { UpdateManager } from './managers/UpdateManager';
+import { UpdateManager } from "./managers/UpdateManager";
 
 dotenv.config();
 Sentry.init({ dsn: process.env.SENTRY_DSN });
 
+/* tslint:disable:object-literal-sort-keys */
 createConnection({
-  type: 'postgres',
+  type: "postgres",
   host: process.env.DB_HOST,
   port: 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [__dirname + '/entity/*.js'],
+  entities: [__dirname + "/entity/*.js"],
   synchronize: false,
 })
   .then(async () => await UpdateManager.init())
-  .catch(error => console.log(error));
+  .catch((error) => console.log(error));
