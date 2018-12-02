0dbef7792a0b6fcf6bb5dbf8e9ac1072b4de3266
Thales Pereira <thalesfp@gmail.com>
2018-12-01 23:46:06 -0200

    Add cron to start packege

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as Sentry from "@sentry/node";
+import * as cron from "cron";
 import * as dotenv from "dotenv";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
@@ -10,23 +11,34 @@ import { UpdateManager } from "./managers/UpdateManager";
 dotenv.config();
 Sentry.init({ dsn: process.env.SENTRY_DSN });
 
-/* tslint:disable:object-literal-sort-keys */
-createConnection({
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  entities: [__dirname + "/entity/*.js"],
-  synchronize: false,
-})
-  .then(async (connection) => {
+const initUpdate = async () => {
+  try {
     const fipeManager = new FipeManager();
     const updateManager = new UpdateManager(fipeManager);
+
+    /* tslint:disable:object-literal-sort-keys */
+    const connection = await createConnection({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + "/entity/*.js"],
+      synchronize: false,
+    });
 
     await updateManager.init(connection);
 
     connection.close();
-  })
-  .catch((error) => logger.error(error));
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+if (process.env.NODE_ENV !== "production") {
+  const job = new cron.CronJob("0 0 5 * * *", initUpdate);
+  job.start();
+} else {
+  initUpdate();
+}
