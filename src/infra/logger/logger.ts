45d4e0add23ce88b34d816defcdd2d35c8d0c8b8
Thales Pereira <thalesfp@gmail.com>
2018-12-02 09:42:18 -0200

    Ajust logger to log in a file on production

--- src/infra/logger/logger.ts
+++ src/infra/logger/logger.ts
@@ -1,6 +1,18 @@
 import * as winston from "winston";
 
-export default winston.createLogger({
-  format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
-  transports: [new winston.transports.Console()],
+const formatPrinter = winston.format.printf((info) => {
+  return `${info.timestamp} ${info.level}: ${info.message}`;
 });
+
+const format = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp(),
+  formatPrinter,
+);
+
+const transports =
+  process.env.NODE_ENV === "production"
+    ? new winston.transports.File({ filename: "production.log" })
+    : new winston.transports.Console();
+
+export default winston.createLogger({ format, transports });
